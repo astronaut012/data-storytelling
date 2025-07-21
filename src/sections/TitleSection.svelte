4f96627f0864984c281884ec19ff2431dfab4ed1
Shivani <shdeo.learn@gmail.com>
2025-07-21 17:23:24 +0530

    Title updated

--- src/sections/TitleSection.svelte
+++ src/sections/TitleSection.svelte
@@ -1,8 +1,8 @@
 <script>
     import TitleCard from "../lib/TitleCard.svelte";
     
-    const title = "Welcome to the KWK Scrollytelling Template";
-    const subtitle = "A journey through data, creativity, and code";
+    const title = "Welcome, Adventurer!";
+    let subtitle = "Scroll below to dive into a journey";
 </script>
 
 <TitleCard {title} {subtitle} />
